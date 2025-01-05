bac9d59e638de29dc67191e03fed166190036f85
tijanaigrutinovic <tijana.igrutinovic@gmail.com>
2025-01-06 00:52:32 +0100

    formating code

--- yorkteahaven/yorkteahaven/static/js/yorkteahaven.js
+++ yorkteahaven/yorkteahaven/static/js/yorkteahaven.js
@@ -3,29 +3,26 @@ const modal = document.getElementById('modal');
 const closeModal = document.getElementById('close');
 const navLinks = document.querySelectorAll('.modal-nav-list a');
 
-// Otvoriti modal kada se klikne na hamburger ikonu
 hamburger.addEventListener('click', () => {
     modal.style.display = 'block'; 
 });
 
-// Zatvoriti modal kada se klikne na "close" dugme
 closeModal.addEventListener('click', () => {
     modal.style.display = 'none'; 
 });
 
-// Zatvoriti modal ako se klikne van njega
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.style.display = 'none';
     }
 });
 
-// Zatvoriti modal kada se klikne na link unutar modala
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         modal.style.display = 'none';
     });
 });
+
 
 //brew tips
 window.addEventListener('scroll', function() {
@@ -46,7 +43,6 @@ window.addEventListener('scroll', function() {
       const parentDiv = this.closest('.brew-tip');
       const fullDescription = parentDiv.querySelector('.full-description');
       
-      // Samo prikazivanje teksta kada se klikne dugme
       if (fullDescription.style.display === 'block' || fullDescription.style.display === '') {
         fullDescription.style.display = 'block';
       }
